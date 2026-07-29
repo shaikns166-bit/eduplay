79fadb06a793dfff90525ef51cc60e1702e4739d
shaikns166-bit <shaikns166@gmail.com>
2026-07-29 20:10:16 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eduplay-pwa-games-v11';
+const CACHE_NAME = 'eduplay-pwa-games-v12';
 
 self.addEventListener('install', event => { 
     event.waitUntil( caches.open(CACHE_NAME).then(cache => cache.addAll([ './', './index.html' ])) ); 
