7171ff3917e67ca88c5b6a6b635ae82935645432
shaikns166-bit <shaikns166@gmail.com>
2026-08-22 11:43:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'eduplay-pwa-games-v158';
+const CACHE_NAME = 'eduplay-pwa-games-v159';
 
 // 1. INSTALLATION: Save the core files immediately
 self.addEventListener('install', event => {
